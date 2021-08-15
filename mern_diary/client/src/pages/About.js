fc030c738e5fe34aa4ed2a4f6b0615700efdf221
k3sha7 <88152359+k3sha7@users.noreply.github.com>
2021-08-15 21:23:50 +0530

    Update About.js

--- mern_diary/client/src/pages/About.js
+++ mern_diary/client/src/pages/About.js
@@ -9,8 +9,12 @@ const About = () => {
     return (
         <div style={style} className="about">
             <h2>About</h2>
-            <p>svdsvcxc dbcb cbxcbvxcsdb dbxcsdchbgsj dhgshdbgvs dsbdv xckb xchb xc bxkjcbj
-                vsdjv kjxc vxcn vxcv 
+            <p>A simple MERN Diary application which can register and login user useing jwt which 
+            is saved useing contextAPI, after logging in can create diary entry,edit and delete.<br/>
+            <br/><b>Tech used</b><br/>
+            react : reacthooks(useState, useEffect, useContext),react-router and axios<br/>
+            nodejs : Express, cors, middleware, router , cors, jwt, bcrypt<br/>
+            mongoDB<br/>
             </p>
             <h2>Routes</h2>
             <li>/login</li>
